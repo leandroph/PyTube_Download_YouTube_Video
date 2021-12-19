d28bec1ad044ccc29c3acd1d1361ec05d8feda6b
Leandro Heck <leandro.h@sou.unijui.edu.br>
2021-12-19 19:56:52 -0300

    alteracao
    Alteração no código usando a nova atualização do Pytube

--- audio.py
+++
@@ -1,5 +0,0 @@
-from youtube_dl import YoutubeDL
-
-audio_downloader = YoutubeDL({'format':'bestaudio'})
-
-audio_downloader.extract_info('https://www.youtube.com/watch?v=G0YHlf7GT1k')
--- download.py
+++ download.py
@@ -1,60 +1,33 @@
-from youtube_dl import YoutubeDL
-import ffmpeg
+from pytube import YouTube
 
-NOME = 'YOUTUBE DOWNLOADER'
-MSG = f'Encerrando o Programa {NOME}'
+# caminho onde salvar o arquivo que sera baixado
+SAVE_PATH = "Downloads"
 
-#https://www.ti-enxame.com/pt/python/baixar-apenas-o-audio-do-video-do-youtube-usando-o-youtube-dl-no-script-python/1050145910/
-#https://dev.to/kalebu/how-to-download-youtube-video-as-audio-using-python-33g9
-def download_video(url):
-    ydl_opts = {'format': 'bestvideo[height<=?720]+bestaudio/best'}
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+# link do vídeo a ser baixado
+link = input("Link do vídeo: ")
 
+try:
+    # criação de objetos usando o YouTube
+    # que foi importado no início
+    yt = YouTube(link)
+except:
+    print("Erro de conexão")  # para lidar com a exceção
 
-def download_audio(url):
-    audio_downloader = YoutubeDL({'format': 'bestaudio',
-                                  'preferredcodec': 'mp3'})
-    audio_downloader.extract_info(url)
+# filtrar se ira baixar video ou audio
+option = input("1 para download do video \n 2 para donload do audio ")
 
-def informacao(url):
-    ydl_opts = {}
+if option == "1":
+    mp4files = yt.streams.get_highest_resolution()
+elif option == "2":
+    mp4files = yt.streams.get_audio_only()
+else:
+    print("Opção Inválida")
+    option = input("1 para download do video"
+                   "2 para donload do audio ")
 
-    with YoutubeDL(ydl_opts) as ydl:
-        meta = ydl.extract_info(url, download=False)
-
-    print('upload date : %s' % (meta['upload_date']))
-    print('uploader    : %s' % (meta['uploader']))
-    print('views       : %d' % (meta['view_count']))
-    print('likes       : %d' % (meta['like_count']))
-    print('dislikes    : %d' % (meta['dislike_count']))
-    print('id          : %s' % (meta['id']))
-    print('format      : %s' % (meta['format']))
-    print('duration    : %s' % (meta['duration']))
-    print('title       : %s' % (meta['title']))
-    print('description : %s' % (meta['description']))
-
-def painel():
-    print('-' * len(NOME))
-    print(NOME)
-    print('-' * len(NOME))
-
-def encerrando_sistema():
-    print('-' * len(MSG))
-    print(MSG)
-    print('-' * len(MSG))
-
-#download_video('https://www.youtube.com/watch?v=G0YHlf7GT1k')
-while True:
-    painel()
-    opcao = input('1 - Video \n2 - Audio\n3 - PARA SAIR\n')
-    link = input('Informe o link link do video do YOUTUBE: ')
-
-    if opcao == '1':
-        download_video(str(link))
-    elif opcao == '2':
-        download_audio(str(link))
-    elif opcao == '3':
-        encerrando_sistema()
-        break
-
+try:
+    # baixando o vídeo
+    mp4files.download(SAVE_PATH)
+except:
+    print("Algum erro!")
+print('Tarefa completa!')
